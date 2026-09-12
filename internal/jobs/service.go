ce7f44d4d9b85f590fe440a594c4d18ff0153e25
Luis <92736550+luisync@users.noreply.github.com>
2026-09-12 22:03:48 +0100

    Added ability to find a job by the ID.

--- internal/jobs/service.go
+++ internal/jobs/service.go
@@ -1,22 +1,34 @@
 package jobs
 
-import "context"
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	repo "github.com/luisync/Job-Queue/internal/adapters/postgresql/sqlc"
+)
 
 // Functions a struct must define to be considered a service.
 type Service interface {
-	ListJobs(ctx context.Context) error
+	ListJobs(ctx context.Context) ([]repo.Job, error)
+	FindJob(ctx context.Context, jobID pgtype.UUID) (repo.Job, error)
 }
 
 // Services depend on the repository.
 type svc struct {
+	repo repo.Querier
 }
 
 // Constructor for creating the services.
-func NewService() Service {
-	return &svc{}
+func NewService(repo repo.Querier) Service {
+	return &svc{repo: repo}
 }
 
 // List all jobs.
-func (s *svc) ListJobs(ctx context.Context) error {
-	return nil
+func (s *svc) ListJobs(ctx context.Context) ([]repo.Job, error) {
+	return s.repo.ListJobs(ctx)
+}
+
+// Find a job by ID.
+func (s *svc) FindJob(ctx context.Context, jobID pgtype.UUID) (repo.Job, error) {
+	return s.repo.FindJobByID(ctx, jobID)
 }
